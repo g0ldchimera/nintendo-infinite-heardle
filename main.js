c5581862ba111a7240b6f99cd80e99823e3b2a9b
g0ldchimera <tombs.mj@gmail.com>
2026-05-31 22:38:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4464,7 +4464,7 @@ var app = (function () {
       p(ccTags, unselectAllTags);
       explainationTags = w("div");
       explainationTags.innerHTML =
-        "You can select tags below to filter musics.";
+        "You can select tags below to filter tracks. Top Tracks are a curated selection of each game's more prominent music so will be easier; Selects is a much narrower selection of between 1 and 7 tracks per game, so easier still. Make sure to untick 'All' if using these filters!";
     }
     return {
       c() {
