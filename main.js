03cae47c015d8ea1d1e9ca77e462ef7dc0ea903b
g0ldchimera <tombs.mj@gmail.com>
2026-06-02 00:03:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1318,10 +1318,14 @@ var app = (function () {
 
     function s(e, t) {
       return e[0][e[7]].isCorrect || e[0][e[7]].isSkipped
-        ? e[0][e[7]].isSkipped
-          ? Te
+        if e[0][e[7]].isSkipped {
+          return Te
           : void 0
-        : Ye;
+        } else if (e[0][e[7]].answer.split(" - ")[1] === e[2].artist) {
+          return GreenCross;
+        } else {
+          return Ye;
+        }
     }
     let i = s(e),
       o = i && i(e);
